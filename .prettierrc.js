fd481b74342a0afcbacf43351a74a8ac56b0107c
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-05 19:28:15 +0900

    feat : init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 80,
+  plugins: [require('prettier-plugin-tailwindcss')],
+};
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      <div className="text-[#f14343]">hello</div>
+    </>
+  );
+}
