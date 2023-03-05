593c1af0cf76e893f0a38a300462eb103047103e
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-06 00:08:23 +0900

    feat : init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import Input from '@/components/common/Input';
+
 export default function Home() {
   return (
     <>
       <div className="text-[#f14343]">hello</div>
+      <Input type="text" placeholder="hello" />
     </>
   );
 }
