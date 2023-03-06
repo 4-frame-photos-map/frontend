af9c5d9f9a385e966db9c243ce62340be6782c3c
thyeone <thyeonee@gmail.com>
2023-03-07 01:53:06 +0900

    feat: getLayout, Hydrate 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
 import '@/styles/globals.css';
+import { NextPage } from 'next';
 import type { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import React, { ReactElement, ReactNode } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
-const queryClient = new QueryClient();
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = React.useState(() => new QueryClient());
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return getLayout(
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>,
   );
 }
