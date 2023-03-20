32d1bdff1be5368a33b5da43466423e558ff45ac
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-21 02:34:30 +0900

    feat : 폰트 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://webfontworld.github.io/pretendard/Pretendard.css"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
