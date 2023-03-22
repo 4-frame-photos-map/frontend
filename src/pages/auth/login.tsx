bff9cd105e0e4a4beb3580d33da949299b9b1600
thyeone <thyeonee@gmail.com>
2023-03-22 14:29:55 +0900

    feat: 로그인 페이지 1차 디자인

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,27 @@
 import PageLayout from '@/components/common/PageLayout';
+import Image from 'next/image';
 
 const Login = () => {
-  return <PageLayout>Login</PageLayout>;
+  return (
+    <PageLayout>
+      <Image
+        src="/svg/login-logo.svg"
+        alt="logo"
+        width={183}
+        height={48}
+        className="absolute left-[96px] top-[337px] h-[48px] w-[183px]"
+      />
+      <div className="absolute left-[38px] top-[504px] flex h-[45px] w-[300px] flex-row items-center justify-center gap-[8px] rounded-[6px] bg-[#FEE500] p-[20px]">
+        <Image src="/svg/kakao.svg" alt="Kakao Icon" width={18} height={18} />
+        <span className="h-[17px] w-[80px] cursor-pointer text-[14.5px] font-normal not-italic leading-[17px] text-[#000000]">
+          카카오 로그인
+        </span>
+      </div>
+      <p className="absolute left-[129px] top-[573px] h-[17px] w-[121px] cursor-pointer text-[14.5px] font-normal not-italic leading-[17px] text-[#000000] underline">
+        로그인 없이 이용하기
+      </p>
+    </PageLayout>
+  );
 };
 
 export default Login;
