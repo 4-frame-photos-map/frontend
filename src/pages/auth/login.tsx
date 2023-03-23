a42c125b8ed49f44a68c4cbda6063d9218aa77c6
thyeone <thyeonee@gmail.com>
2023-03-23 15:02:16 +0900

    fix: next/image priority 오류 수정

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,15 +4,21 @@ import Image from 'next/image';
 const Login = () => {
   return (
     <AuthLayout>
-      <Image src="/svg/login-logo.svg" alt="logo" width={183} height={48} />
+      <Image
+        src="/svg/login-logo.svg"
+        alt="logo"
+        width={183}
+        height={48}
+        priority={true}
+      />
       <div className="mx-[35px] mt-[140px] flex h-[45px] items-center justify-center gap-[8px] rounded-[6px] bg-[#FEE500] px-[80px]">
         <Image src="/svg/kakao.svg" alt="Kakao Icon" width={18} height={18} />
-        <p className="cursor-pointer whitespace-nowrap text-[14.5px] font-normal not-italic leading-[17px] text-[#000000]">
+        <p className="cursor-pointer whitespace-nowrap text-label2 font-normal not-italic leading-[17px] text-black">
           카카오 로그인
         </p>
       </div>
       <div>
-        <p className="mt-[24px] h-[17px] w-[121px] cursor-pointer text-[14.5px] font-normal not-italic leading-[17px] text-[#000000] underline">
+        <p className="mt-[24px] h-[17px] w-[121px] cursor-pointer text-label2 font-normal not-italic leading-[17px] text-black underline">
           로그인 없이 이용하기
         </p>
       </div>
