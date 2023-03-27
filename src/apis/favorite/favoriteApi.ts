b75dbc382c9e5371150e1590a5d5a76d92a1b50c
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-27 22:15:00 +0900

    feat : favorite api 추가

---
+++ src/apis/favorite/favoriteApi.ts
@@ -0,0 +1,22 @@
+import instance from '@apis/instance';
+
+class FavoriteApi {
+  getFavorites = async (sort: string): Promise<Favorite> => {
+    const { data } = await instance.get(`/favorites&${sort}`);
+    return data;
+  };
+
+  postFavorites = async (shopId: number): Promise<TResponse> => {
+    const { data } = await instance.post(`/favorites/${shopId}`);
+    return data;
+  };
+
+  delFavorites = async (shopId: number): Promise<TResponse> => {
+    const { data } = await instance.delete(`/favorites/${shopId}`);
+    return data;
+  };
+}
+
+const favoriteApi = new FavoriteApi();
+
+export default favoriteApi;
