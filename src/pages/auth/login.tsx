f50bb131944e0344f80ee8d245c0ba6e225767df
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-27 22:10:48 +0900

    feat : 소셜로그인 링크 경로 추가

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,7 +1,14 @@
 import AuthLayout from '@components/common/AuthLayout';
+import { CONFIG } from '@config';
+import { deleteToken } from '@utils/token';
 import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect } from 'react';
 
 const Login = () => {
+  useEffect(() => {
+    deleteToken();
+  }, []);
   return (
     <AuthLayout>
       <Image
@@ -11,12 +18,15 @@ const Login = () => {
         height={48}
         priority={true}
       />
-      <div className="mx-[35px] mt-[140px] flex h-[45px] items-center justify-center gap-[8px] rounded-[6px] bg-[#FEE500] px-[80px]">
+      <Link
+        href={`https://kauth.kakao.com/oauth/authorize?client_id=${CONFIG.API_KEYS.LOGIN}&redirect_uri=${CONFIG.LOCAL}/auth/kakao&response_type=code`}
+        className="mx-[35px] mt-[140px] flex h-[45px] items-center justify-center gap-[8px] rounded-[6px] bg-[#FEE500] px-[80px]"
+      >
         <Image src="/svg/kakao.svg" alt="Kakao Icon" width={18} height={18} />
         <p className="cursor-pointer whitespace-nowrap text-label2 font-normal not-italic leading-[17px] text-black">
           카카오 로그인
         </p>
-      </div>
+      </Link>
       <div>
         <p className="mt-[24px] h-[17px] w-[121px] cursor-pointer text-label2 font-normal not-italic leading-[17px] text-black underline">
           로그인 없이 이용하기
