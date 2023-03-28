c9667faf375160702df47e1098c6c486e12e7db0
thyeone <thyeonee@gmail.com>
2023-03-29 00:47:00 +0900

    feat: Home 관련 컴포넌트 import

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,16 @@
 import NavBar from '@components/common/NavBar';
 import PageLayout from '@components/common/PageLayout';
+import Category from '@components/home/Category';
+import Research from '@components/home/Research';
+import Tracker from '@components/home/Tracker';
 
 const Home = () => {
   return (
     <PageLayout>
       <NavBar area="지도 지역명" isRight={true} />
-      Home
+      <Category />
+      <Research />
+      <Tracker />
     </PageLayout>
   );
 };
