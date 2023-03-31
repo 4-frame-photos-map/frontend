86698fc24c0f7aacb9215afd44bafd563696ccfd
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-31 21:03:37 +0900

    refactor : favorite 타입 수정

--- src/apis/favorite/favorite.d.ts
+++ src/apis/favorite/favorite.d.ts
@@ -1,12 +1,8 @@
-type FavoriteProps = {
+type Favorite = {
   id: number;
   shop: {
     id: number;
     placeName: string;
     roadAddressName: string;
   };
-};
-
-type Favorite = TResponse & {
-  result: FavoriteProps[];
 };
