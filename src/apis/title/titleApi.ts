75096b48afc75ab915b70f4f74f2ae4f9b258559
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-31 21:06:27 +0900

    refactor : api return값 수정

--- src/apis/title/titleApi.ts
+++ src/apis/title/titleApi.ts
@@ -3,17 +3,17 @@ import instance from '@apis/instance';
 class TitleApi {
   getTitle = async (id: number): Promise<MemberTitle> => {
     const { data } = await instance.get(`/member-titles/${id}`);
-    return data;
+    return data.result;
   };
 
   getAllTitles = async (): Promise<MemberTitle> => {
     const { data } = await instance.get(`/member-titles`);
-    return data;
+    return data.result;
   };
 
-  getShopTitles = async (shopId: number): Promise<ShopTitle> => {
+  getShopTitles = async (shopId: number): Promise<ShopTitle[]> => {
     const { data } = await instance.get(`/shop-title-logs/${shopId}`);
-    return data;
+    return data.result;
   };
 }
 
