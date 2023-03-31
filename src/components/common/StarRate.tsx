4f2f8e013e9edc2373c9d017ad5e361c0b561034
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-31 21:10:36 +0900

    feat : 별점 컴포넌트 생성

---
+++ src/components/common/StarRate.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+type StarRateProps = {
+  color?: boolean;
+  rate: number;
+};
+
+const StarRate = ({ color = false, rate }: StarRateProps) => {
+  return (
+    <div className="flex">
+      {Array(Math.floor(rate))
+        .fill(2)
+        .map((_, idx) => (
+          <Image
+            key={idx}
+            src={
+              color ? `/svg/checked_star.svg` : `/svg/checked_star_black.svg`
+            }
+            width={color ? 25 : 18}
+            height={color ? 25 : 18}
+            alt="별"
+          />
+        ))}
+      {rate % 1 !== 0 && (
+        <Image
+          src={color ? `/svg/half_star.svg` : `/svg/half_star_black.svg`}
+          width={color ? 25 : 18}
+          height={color ? 25 : 18}
+          alt="별"
+        />
+      )}
+      {Array(Math.floor(5 - rate))
+        .fill(2)
+        .map((_, idx) => (
+          <Image
+            key={idx}
+            src={color ? `/svg/blank_star.svg` : `/svg/blank_star_black.svg`}
+            width={color ? 25 : 18}
+            height={color ? 25 : 18}
+            alt="별"
+          />
+        ))}
+    </div>
+  );
+};
+
+export default StarRate;
