28329fee892aa52db742ff9aab0cb2c639eb84dd
GeonwooShin <rjsdnslawkd@naver.com>
2023-04-09 17:38:33 +0900

    feat : 스크립트 로드방식 수정

---
+++ src/components/common/Scripts.tsx
@@ -0,0 +1,50 @@
+import Script from 'next/script';
+import { CONFIG } from '@config';
+import { RefObject } from 'react';
+
+type ScriptsProps = {
+  shopInfo: ShopDetail;
+  mapContainer: RefObject<HTMLDivElement>;
+};
+
+const Scripts = ({ shopInfo, mapContainer }: ScriptsProps) => {
+  return (
+    <>
+      <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${CONFIG.API_KEYS.MAP}&libraries=services,clusterer&autoload=false`}
+        onReady={() => {
+          const { kakao } = window;
+          kakao.maps.load(() => {
+            const options = {
+              center: new kakao.maps.LatLng(
+                Number(shopInfo?.latitude),
+                Number(shopInfo?.longitude),
+              ),
+              level: 2,
+            };
+            const map = new kakao.maps.Map(mapContainer.current, options);
+            const imageSrc = '/svg/marker.svg';
+            const imageSize = new kakao.maps.Size(32, 32);
+            const imageOption = { offset: new kakao.maps.Point(20, 20) };
+            const markerImage = new kakao.maps.MarkerImage(
+              imageSrc,
+              imageSize,
+              imageOption,
+            );
+            const markerPosition = new kakao.maps.LatLng(
+              Number(shopInfo?.latitude),
+              Number(shopInfo?.longitude),
+            );
+            const marker = new kakao.maps.Marker({
+              position: markerPosition,
+              image: markerImage,
+            });
+            marker.setMap(map);
+          });
+        }}
+      />
+    </>
+  );
+};
+
+export default Scripts;
