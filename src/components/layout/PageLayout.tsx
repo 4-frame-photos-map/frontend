5b6baa0acf5952f489fc30827ab97a2e0e98cc11
GeonwooShin <rjsdnslawkd@naver.com>
2023-04-09 17:39:07 +0900

    refactor : layout 파일 구조 수정

---
+++ src/components/layout/PageLayout.tsx
@@ -0,0 +1,23 @@
+import tw from 'tailwind-styled-components';
+import React from 'react';
+import Menu from '@components/common/Menu';
+
+type PageLayoutProps = {
+  children: React.ReactNode;
+  className?: React.ComponentProps<'div'>['className'];
+};
+
+const PageLayoutBox = tw.div`
+relative h-full w-full min-h-screen max-w-[375px] bg-bg-primary
+`;
+
+const PageLayout = ({ children, ...rest }: PageLayoutProps) => {
+  return (
+    <PageLayoutBox {...rest}>
+      {children}
+      <Menu />
+    </PageLayoutBox>
+  );
+};
+
+export default PageLayout;
