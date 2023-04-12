beaa71db76b1d3aadbbf765fe5e3d81b7fc246ab
GeonwooShin <rjsdnslawkd@naver.com>
2023-04-12 21:41:14 +0900

    refactor : handler type 추가

--- src/components/home/TrackerButton.tsx
+++ src/components/home/TrackerButton.tsx
@@ -1,9 +1,16 @@
 import Image from 'next/image';
 
-const TrackerButton = () => {
+type TrackerProps = {
+  onClick: () => void;
+};
+
+const TrackerButton = ({ onClick }: TrackerProps) => {
   return (
     <div className="right-0 mr-[25px] mb-[14px] flex justify-end">
-      <button className="h-[44px] w-[44px] rounded-full bg-primary-normal shadow-tracker">
+      <button
+        onClick={onClick}
+        className="h-[44px] w-[44px] rounded-full bg-primary-normal shadow-tracker"
+      >
         <Image
           src="/svg/home/track.svg"
           alt="tracker"
