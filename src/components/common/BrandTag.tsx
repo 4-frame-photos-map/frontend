dc80a75efe488aa9249542f580ce0e23d8431b61
GeonwooShin <rjsdnslawkd@naver.com>
2023-04-14 23:53:58 +0900

    refactor : 기타 지점 text color 수정

--- src/components/common/BrandTag.tsx
+++ src/components/common/BrandTag.tsx
@@ -6,18 +6,22 @@ type BrandTagProps = {
 
 const BrandTag = ({ name }: BrandTagProps) => {
   const brandNames = ['인생네컷', '하루필름', '포토이즘', '포토그레이'];
+  console.log(brandNames.includes(name));
   return (
     <BadgeContainer
-      className={
-        `bg-brand-others ${name?.includes(brandNames[0]) && 'bg-brand-pink'} ${
-          name?.includes(brandNames[1]) && 'bg-brand-blue'
-        } ${name?.includes(brandNames[2]) && 'bg-brand-black'}  ${
-          name?.includes(brandNames[3]) && 'bg-brand-gray'
-        }` || 'bg-brand-others'
-      }
+      className={`bg-brand-others ${
+        name?.includes(brandNames[0]) && 'bg-brand-pink'
+      } ${name?.includes(brandNames[1]) && 'bg-brand-blue'} ${
+        name?.includes(brandNames[2]) && 'bg-brand-black'
+      }  ${name?.includes(brandNames[3]) && 'bg-brand-gray'}`}
     >
       {
-        <span className="text-center text-[10px] text-white">
+        <span
+          className={`text-center text-[10px] ${
+            brandNames.find((brandName) => name?.includes(brandName)) &&
+            'text-white'
+          } text-black`}
+        >
           {brandNames.find((brandName) => name?.includes(brandName)) || '기타'}
         </span>
       }
