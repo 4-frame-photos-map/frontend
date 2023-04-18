5312f8f0dc5a0c5ed6c32e7947a38e360c0322d9
thyeone <thyeonee@gmail.com>
2023-04-18 17:35:15 +0900

    refactor: 비로그인 모달 추가

--- src/components/wish/FavoriteButton.tsx
+++ src/components/wish/FavoriteButton.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { queryClient } from 'pages/_app';
 import { useDeleteFavorite } from '@hooks/mutations/useDeleteFavorite';
 import { usePostFavorites } from '@hooks/mutations/usePostFavorite';
 import { useGetFavorite } from '@hooks/queries/useGetFavorite';
 import Modal from '@components/common/Modal';
 import ToastMessage from '@components/common/ToastMessage';
+import tw from 'tailwind-styled-components';
+import { getLocalStorage } from '@utils/localStorage';
 
 type FavortieButtonProps = {
   shopId: number;
@@ -21,6 +23,7 @@ const FavoriteButton = ({ shopId, isWish }: FavortieButtonProps) => {
   ]);
 
   const [isModal, setIsModal] = useState<boolean>(false);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
   const [toast, setToast] = useState<boolean>(false);
 
   const handleAddFavorite = (shopId: number) => {
@@ -77,6 +80,15 @@ const FavoriteButton = ({ shopId, isWish }: FavortieButtonProps) => {
               handleToast();
             }}
           />
+        ) : !getLocalStorage('@token') ? (
+          <Image
+            src="/svg/wish/lined-bookmark.svg"
+            width={24}
+            height={24}
+            alt="bookmark"
+            className="z-[900] cursor-pointer"
+            onClick={() => setIsLogin(true)}
+          />
         ) : (
           <Image
             src="/svg/wish/lined-bookmark.svg"
@@ -89,7 +101,7 @@ const FavoriteButton = ({ shopId, isWish }: FavortieButtonProps) => {
         ))}
 
       {isModal && (
-        <div className="absolute top-0 left-0 h-full w-full">
+        <ModalLayout>
           <Modal
             isModal={isModal}
             isKakao={false}
@@ -105,7 +117,19 @@ const FavoriteButton = ({ shopId, isWish }: FavortieButtonProps) => {
               setIsModal(false);
             }}
           />
-        </div>
+        </ModalLayout>
+      )}
+      {isLogin && (
+        <ModalLayout>
+          <Modal
+            isModal={true}
+            isKakao={true}
+            title="로그인 상태가 아니에요!"
+            message="해당 기능은 카카오톡 로그인을 하셔야 이용가능한 기능이에요. 로그인 하시겠어요?"
+            left="아니요"
+            leftEvent={() => setIsLogin(false)}
+          />
+        </ModalLayout>
       )}
 
       {toast && isSuccess && (
@@ -128,5 +152,9 @@ const FavoriteButton = ({ shopId, isWish }: FavortieButtonProps) => {
     </>
   );
 };
+
+const ModalLayout = tw.div`
+absolute top-0 left-0 w-full h-full
+`;
 
 export default FavoriteButton;
