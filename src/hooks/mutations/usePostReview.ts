14754ed394568c699de27acaf9588b6eb7199015
GeonwooShin <rjsdnslawkd@naver.com>
2023-04-26 16:28:49 +0900

    refactor : type 수정

--- src/hooks/mutations/usePostReview.ts
+++ src/hooks/mutations/usePostReview.ts
@@ -2,20 +2,11 @@ import reviewApi from '@apis/review/reviewApi';
 import { useMutation } from 'react-query';
 import { useRouter } from 'next/router';
 
-type Data = (
-  | number
-  | {
-      star_rating: number;
-      content: string;
-      item: string;
-      purity: string;
-      retouch: string;
-    }
-)[];
+type Data = (number | ReviewInfoProps)[];
 
 const usePostReview = () => {
   const router = useRouter();
-  return useMutation<TResponse, Error, Data>(
+  return useMutation<void, Error, Data>(
     'usePostReview',
     (formData: any) => reviewApi.postReview(formData[0], formData[1]),
     {
