5eb18b6d76c88d80e38f2a8d642fae2120c13b37
GeonwooShin <rjsdnslawkd@naver.com>
2023-05-10 18:43:32 +0900

    refactor : 리뷰 유무에 따른 처리

--- src/pages/my/reviews/index.tsx
+++ src/pages/my/reviews/index.tsx
@@ -17,7 +17,7 @@ const Reviews = () => {
   const { data: reviews } = useGetAllUserReviews();
   const { mutate: deleteReview } = useDeleteReview();
   return (
-    <PageLayout className="bg-white">
+    <PageLayout className="bg-bg-primary">
       {isModal && (
         <ModalLayout>
           <Modal
@@ -35,52 +35,67 @@ const Reviews = () => {
         </ModalLayout>
       )}
       <NavBar centerTitle="내 후기" isLeft={true} />
-      <ul className="my-[52px]">
-        {reviews?.map(({ review_info, shop_info }, idx) => (
-          <ContentBox key={idx}>
-            <div className="flex justify-between">
-              <div className="flex flex-col">
-                <span className="text-caption1">
-                  <BrandTag name={shop_info?.brand} />
-                </span>
-                <div className="mb-4 text-title1 font-semibold">
-                  {shop_info.place_name}
+      {reviews && reviews.length ? (
+        <ul className="my-[52px]">
+          {reviews?.map(({ review_info, shop_info }, idx) => (
+            <ContentBox key={idx}>
+              <div className="flex justify-between">
+                <div className="flex flex-col">
+                  <span className="text-caption1">
+                    <BrandTag name={shop_info?.brand} />
+                  </span>
+                  <div className="mb-4 text-title1 font-semibold">
+                    {shop_info.place_name}
+                  </div>
                 </div>
+                <BtnContainer>
+                  <BtnBox
+                    onClick={() =>
+                      router.replace(
+                        `/my/reviews/edit?reviewId=${review_info.id}`,
+                      )
+                    }
+                  >
+                    <Image
+                      src={'/svg/pen.svg'}
+                      width={18}
+                      height={16}
+                      alt="펜"
+                    />
+                  </BtnBox>
+                  <BtnBox
+                    onClick={() => {
+                      setReviewId(review_info.id);
+                      setIsModal(true);
+                    }}
+                  >
+                    <Image
+                      src={'/svg/trash_can.svg'}
+                      width={18}
+                      height={16}
+                      alt="쓰레기통"
+                    />
+                  </BtnBox>
+                </BtnContainer>
               </div>
-              <BtnContainer>
-                <BtnBox
-                  onClick={() =>
-                    router.push(`/my/reviews/edit?reviewId=${review_info.id}`)
-                  }
-                >
-                  <Image src={'/svg/pen.svg'} width={18} height={16} alt="펜" />
-                </BtnBox>
-                <BtnBox
-                  onClick={() => {
-                    setReviewId(review_info.id);
-                    setIsModal(true);
-                  }}
-                >
-                  <Image
-                    src={'/svg/trash_can.svg'}
-                    width={18}
-                    height={16}
-                    alt="쓰레기통"
-                  />
-                </BtnBox>
-              </BtnContainer>
-            </div>
-            <ReviewItem
-              create_date={review_info.create_date}
-              star_rating={review_info.star_rating}
-              content={review_info.content}
-              purity={review_info.purity}
-              retouch={review_info.retouch}
-              item={review_info.item}
-            />
-          </ContentBox>
-        ))}
-      </ul>
+              <ReviewItem
+                create_date={review_info.create_date}
+                star_rating={review_info.star_rating}
+                content={review_info.content}
+                purity={review_info.purity}
+                retouch={review_info.retouch}
+                item={review_info.item}
+              />
+            </ContentBox>
+          ))}
+        </ul>
+      ) : (
+        <div className="flex h-[100vh] items-center justify-center">
+          <span className="text-title2 text-text-alternative">
+            작성한 리뷰가 없습니다.
+          </span>
+        </div>
+      )}
     </PageLayout>
   );
 };
