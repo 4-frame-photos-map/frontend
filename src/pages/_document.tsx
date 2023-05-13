7598e743edf6c396ab3da9e4efac6d13b02a76a5
thyeone <thyeonee@gmail.com>
2023-05-14 01:42:46 +0900

    feat: meta 데이터 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,11 +4,21 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, maximum-scale=1.0, user-scalable=no"
+        />
         <meta
           name="description"
+          content="나와 가까운 네컷 사진 포토부스를 찾아보세요, 네컷 지도"
+        />
+        <meta
+          name="og:description"
           content="나와 가까운 네컷 사진 포토부스를 찾아보세요. 네컷 지도"
         />
+        <meta property="og:title" content="네컷지도" />
+        <meta property="og:type" content="website" />
+        <meta property="og:article:author" content="네컷지도" />
         <link
           href="https://webfontworld.github.io/pretendard/Pretendard.css"
           rel="stylesheet"
