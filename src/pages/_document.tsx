a54c6b98b76c30ed0375e8cd65a508f531321169
thyeone <thyeonee@gmail.com>
2023-05-13 23:34:15 +0900

    feat: 공통적으로 들어가는 meta 태그 적용

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="나와 가까운 네컷 사진 포토부스를 찾아보세요. 네컷 지도"
+        />
         <link
           href="https://webfontworld.github.io/pretendard/Pretendard.css"
           rel="stylesheet"
