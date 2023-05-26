8b4ec95c9a973567fe541b343568d4449555c611
thyeone <thyeonee@gmail.com>
2023-05-27 02:03:14 +0900

    refactor: standard, content 응답값 추가

--- src/apis/title/title.d.ts
+++ src/apis/title/title.d.ts
@@ -3,6 +3,8 @@ type MemberTitle = {
   main_member_title: {
     id: number;
     name: string;
+    standard: string;
+    content: string;
     image_url: string;
     is_holding: boolean;
     is_main: boolean;
@@ -11,6 +13,8 @@ type MemberTitle = {
     {
       id: number;
       name: string;
+      standard: string;
+      content: string;
       image_url: string;
       is_holding: boolean;
       is_main: boolean;
