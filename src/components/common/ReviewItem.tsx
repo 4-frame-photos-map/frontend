ccf055862e2170a0ddbd5e76f7dfc0e280472bd3
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-04 15:06:39 +0900

    chore: nested li 수정

--- src/components/common/ReviewItem.tsx
+++ src/components/common/ReviewItem.tsx
@@ -24,79 +24,81 @@ const ReviewItem = ({
   member_info,
 }: ReviewItemProps) => {
   return (
-    <ReviewBox>
-      <ReviewInfo>
-        <div className="flex items-center">
-          <StarRate rate={star_rating} />
-          {member_info && (
-            <div className="pl-[6px]">{member_info.nickname}</div>
-          )}
-          <div className="ml-2">
-            {member_info?.main_member_title && (
-              <TitleBadge
-                className="h-fit w-fit px-2 text-[9px]"
-                name={member_info.main_member_title as string}
-              />
+    <div className="pb-3 text-text-normal">
+      <ReviewBox>
+        <ReviewInfo>
+          <div className="flex items-center">
+            <StarRate rate={star_rating} />
+            {member_info && (
+              <div className="pl-[6px]">{member_info.nickname}</div>
             )}
+            <div className="ml-2">
+              {member_info?.main_member_title && (
+                <TitleBadge
+                  className="h-fit w-fit px-2 text-[9px]"
+                  name={member_info.main_member_title as string}
+                />
+              )}
+            </div>
           </div>
-        </div>
-        <div>{dateFormat(create_date)}</div>
-      </ReviewInfo>
-      <ReviewValueBox>
-        {purity !== 'UNSELECTED' ? (
-          <ReviewValue>
-            <Image
-              src={'/svg/checkbox_disable.svg'}
-              width={18}
-              height={18}
-              alt={purity === 'GOOD' ? '청결상태 좋음' : '청결상태 나쁨'}
-            />
-            <span className="ml-1">
-              {purity === 'GOOD' ? '청결상태 좋음' : '청결상태 나쁨'}
-            </span>
-          </ReviewValue>
-        ) : (
-          <></>
-        )}
-        {retouch !== 'UNSELECTED' ? (
-          <ReviewValue>
-            <Image
-              src={'/svg/checkbox_disable.svg'}
-              width={18}
-              height={18}
-              alt={retouch === 'GOOD' ? '보정정도 높음' : '보정정도 낮음'}
-            />
-            <span className="ml-1">
-              {retouch === 'GOOD' ? '보정정도 높음' : '보정정도 낮음'}
-            </span>
-          </ReviewValue>
-        ) : (
-          <></>
-        )}
-        {item !== 'UNSELECTED' ? (
-          <ReviewValue>
-            <Image
-              src={'/svg/checkbox_disable.svg'}
-              width={18}
-              height={18}
-              alt={item === 'GOOD' ? '악세사리 많음' : '악세사리 적음'}
-            />
-            <span className="ml-1">
-              {item === 'GOOD' ? '악세사리 많음' : '악세사리 적음'}
-            </span>
-          </ReviewValue>
-        ) : (
-          <></>
-        )}
-      </ReviewValueBox>
-      <ReviewContent>{content}</ReviewContent>
-      <div></div>
-    </ReviewBox>
+          <div>{dateFormat(create_date)}</div>
+        </ReviewInfo>
+        <ReviewValueBox>
+          {purity !== 'UNSELECTED' ? (
+            <ReviewValue>
+              <Image
+                src={'/svg/checkbox_disable.svg'}
+                width={18}
+                height={18}
+                alt={purity === 'GOOD' ? '청결상태 좋음' : '청결상태 나쁨'}
+              />
+              <span className="ml-1">
+                {purity === 'GOOD' ? '청결상태 좋음' : '청결상태 나쁨'}
+              </span>
+            </ReviewValue>
+          ) : (
+            <></>
+          )}
+          {retouch !== 'UNSELECTED' ? (
+            <ReviewValue>
+              <Image
+                src={'/svg/checkbox_disable.svg'}
+                width={18}
+                height={18}
+                alt={retouch === 'GOOD' ? '보정정도 높음' : '보정정도 낮음'}
+              />
+              <span className="ml-1">
+                {retouch === 'GOOD' ? '보정정도 높음' : '보정정도 낮음'}
+              </span>
+            </ReviewValue>
+          ) : (
+            <></>
+          )}
+          {item !== 'UNSELECTED' ? (
+            <ReviewValue>
+              <Image
+                src={'/svg/checkbox_disable.svg'}
+                width={18}
+                height={18}
+                alt={item === 'GOOD' ? '악세사리 많음' : '악세사리 적음'}
+              />
+              <span className="ml-1">
+                {item === 'GOOD' ? '악세사리 많음' : '악세사리 적음'}
+              </span>
+            </ReviewValue>
+          ) : (
+            <></>
+          )}
+        </ReviewValueBox>
+        <ReviewContent>{content}</ReviewContent>
+        <div></div>
+      </ReviewBox>
+    </div>
   );
 };
 
-const ReviewBox = tw.li`
-rounded-[20px] p-4 shadow-[0_8px_20px_0px_rgba(0,0,0,0.1)] list-none mb-3 text-text-normal
+const ReviewBox = tw.div`
+rounded-[20px] p-4 shadow-[0_8px_20px_0px_rgba(0,0,0,0.1)]
 `;
 const ReviewInfo = tw.div`
 flex justify-between text-caption2
